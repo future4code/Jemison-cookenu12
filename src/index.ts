280522ebf9c8b45fb42913a1d45795554ef52f62
karolina-azevedo <karolinam.azevedo@yahoo.com.br>
2023-02-27 21:54:41 -0300

    criacao-tabelas-inicio-endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
-
 import cors from 'cors'
+import { ping } from "./endpoints/ping"
+import { createClient } from "./endpoints/createClient"
 
 const app = express()
 
@@ -11,3 +12,7 @@ app.use(cors())
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
 });
+
+app.get("/ping", ping);
+
+app.post("/client", createClient)
